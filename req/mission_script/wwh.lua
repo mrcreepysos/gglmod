2e43c30291842b89432e32c9d2c1ea9fc58141e0
mrcreepysos <59087849+mrcreepysos@users.noreply.github.com>
2022-12-15 17:01:35 +0300

    presumably fix crash on alaskan

---
+++ req/mission_script/wwh.lua
@@ -0,0 +1,22 @@
+return {
+    [100504] = {
+		values = {
+			enabled = false
+		}
+	},
+    [100507] = {
+		values = {
+			enabled = false
+		}
+	},
+    [100508] = {
+		values = {
+			enabled = false
+		}
+	},
+    [100509] = {
+		values = {
+			enabled = false
+		}
+	}
+}
